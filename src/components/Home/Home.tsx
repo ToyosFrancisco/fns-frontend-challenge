f1cb27d8ca859ae8eaf2c21e88f7f5159740d09b
FranciscoToyos <f.toyos@waia.ar>
2022-11-03 02:59:37 -0300

    [Delete] - ftrs (Welcome) default Finalis. [ADD] - do challenge

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,77 @@
-import React from 'react';
-import { Container } from '@chakra-ui/react';
-import { Welcome } from '../';
+// react hooks
+import { useState, useEffect } from "react";
+
+// components
+import { Header, Badges, Widget, Footer, Paragraph } from "../../modules";
+
+// Styled
+import * as Style from "./HomeStyled";
+
+// Rsuite
+import { Container, Content, FlexboxGrid, Grid, Panel } from "rsuite";
+
+// fetch
+import { fetchData } from "../../services";
+
+// default imports finalis
+//import { Container } from '@chakra-ui/react';
+//import { Welcome } from '../';
+
+// interfaces
+import { IData } from "../../interfaces";
 
 export const Home = () => {
+  // state's
+  const [data, setData] = useState<IData[]>([]);
+
+  // init
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData();
+
+      setData(data);
+    })();
+  }, []);
+
   return (
-    <Container py="16" h="full">
-      {/* ----------- Delete this component and place yours here -------- */}
-      <Welcome />
-      {/* ----------- Delete this component and place yours here -------- */}
+    <Container>
+      <Content>
+        <FlexboxGrid justify="center" align="middle">
+          <Style.WrpContent>
+            <Panel bordered bodyFill>
+              <Style.WrapperPanel>
+                <Style.Body>
+                  {data.map((data, idx) => (
+                    <Header title={data.title} key={idx} />
+                  ))}
+
+                  {data.map(({ status }: IData, idx) => {
+                    return <Badges status={status} key={idx} />;
+                  })}
+
+                  {data.map((data, idx) => (
+                    <Paragraph text={data.text} key={idx} />
+                  ))}
+
+                  <Grid>
+                    <Style.ContainerWidget>
+                      {data.map(({ metadata }: IData, idx) => {
+                        return <Widget metadata={metadata} key={idx} />;
+                      })}
+                    </Style.ContainerWidget>
+                  </Grid>
+                </Style.Body>
+
+                <Style.Divider />
+
+                <Style.BodyFooter>
+                  <Footer />
+                </Style.BodyFooter>
+              </Style.WrapperPanel>
+            </Panel>
+          </Style.WrpContent>
+        </FlexboxGrid>
+      </Content>
     </Container>
   );
 };
