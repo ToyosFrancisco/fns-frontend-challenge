996b836b61309e64bcaf75fcd52d65347d2e7677
FranciscoToyos <f.toyos@waia.ar>
2022-11-03 02:58:02 -0300

    [ADD] new styled , edit ones

---
+++ src/components/Home/HomeStyled.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const WrpContent = styled.div`
+  margin: 76px;
+
+  width:700px;
+
+  @media (max-width: 426px) {
+    margin:40px;
+    width:100%;
+    padding:10px;
+  };
+
+  @media (max-width: 320px) {
+    margin:20px;
+    padding:10px;
+  };
+
+`;
+export const WrapperPanel = styled.div`
+  background: #ffffff;
+  box-shadow: 0px 8px 60px rgba(35, 31, 82, 0.15);
+  border-radius: 6px;
+  
+  
+`;
+
+export const Divider = styled.hr`
+  color: #EDEDF7;
+  heigth: 1px;
+  margin: 20px;
+`;
+
+export const ContainerWidget = styled.div`
+  display:flex;
+  justify-content:flex-start;
+
+  @media (max-width: 768px) {
+    flex-direction:column
+  }
+`
+
+export const Body = styled.div`
+  padding:32px;
+`
+export const BodyFooter = styled.div`
+  padding:32px;
+
+
+  @media (max-width: 550px) {
+    padding:15px;
+  }
+`
