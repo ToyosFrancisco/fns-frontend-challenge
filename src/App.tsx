9ac92292dfcf05f3c6fcf3c6ebba7389b8ecc7df
FranciscoToyos <f.toyos@waia.ar>
2022-11-03 03:02:16 -0300

    [Edit a do] some actions - Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { Home, Layout } from './components';
+import React from "react";
+
+// UI
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import "rsuite/dist/styles/rsuite-default.css";
+
+
+// components
+import { Home, Layout } from "./components";
+
+
 
 function App() {
   return (
-    <ChakraProvider theme={extendTheme({
-      styles: {
-        global: {
-          body: {
-            fontFamily: 'effra'
-          }
-        }
-      }
-    })}>
-      <Layout>
-        <Home />
-      </Layout>
-    </ChakraProvider>
+   
+      <ChakraProvider
+        theme={extendTheme({
+          styles: {
+            global: {
+              body: {
+                fontFamily: "effra",
+              },
+            },
+          },
+        })}
+      >
+        <Layout>
+          <Home />
+        </Layout>
+      </ChakraProvider>
+   
   );
 }
 
